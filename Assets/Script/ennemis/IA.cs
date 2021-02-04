9dc682b67f2f896ec8679376369bcb7102aff8f3
Camille Bengadi <camillebengadi@gmail.com>
2021-02-04 03:08:24 +0100

    IA ennemi
    ajout du comportememt de suivi du joueur

---
+++ Assets/Script/ennemis/IA.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class IA : MonoBehaviour
+{
+    private GameObject targetedPlayer;
+    private NavMeshAgent agentEnnemi;
+
+    private void Awake()
+    {
+       agentEnnemi = GetComponent<NavMeshAgent>();
+       targetedPlayer = GameObject.FindGameObjectWithTag("Player");
+    }
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        agentEnnemi.SetDestination(targetedPlayer.transform.position);
+    }
+}
