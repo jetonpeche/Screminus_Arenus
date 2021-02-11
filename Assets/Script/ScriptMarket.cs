683dc02b88f37123216678b2f0bb6d9fd5c6efde
Alex Le Fourn <alexlefourn44@gmail.com>
2021-02-11 16:04:50 -0500

    Marché
    Ajout des models des armes et du marchés pour la selection des armes

---
+++ Assets/Script/ScriptMarket.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class ScriptMarket : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
+    Material onOverMaterial;
+    // Start is called before the first frame update
+    void Start()
+    {
+        onOverMaterial = Resources.Load<Material>("Material/WeaponShopBackgroundOnOver");
+    }
+
+    public void OnPointerEnter(PointerEventData e)
+    {
+        Debug.Log("Mouse Enter");
+        Debug.Log(GetComponent<RawImage>().IsActive());
+        GetComponent<RawImage>().enabled = true;
+    }
+
+    public void OnPointerExit(PointerEventData e)
+    {
+        Debug.Log("Mouse Exit");
+        GetComponent<RawImage>().enabled = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+}
